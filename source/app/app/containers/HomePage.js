e70ce5d5aabade8aad738af6d116116ba2b79b0e
Ikbal Kazar <mikbalkazar@gmail.com>
2019-10-26 13:24:57 +0300

    connect via connector

--- source/app/app/containers/HomePage.js
+++ source/app/app/containers/HomePage.js
@@ -1,6 +1,7 @@
 // @flow
 import React, { Component } from 'react';
 import Peer from 'simple-peer';
+import WebSocket from 'ws';
 
 type Props = {};
 
@@ -8,28 +9,62 @@ export default class HomePage extends Component<Props> {
   props: Props;
 
   componentDidMount() {
+    const id = `${Math.floor(Math.random() * 100000)}`;
+    const ws = new WebSocket('ws://0d5b3915.ngrok.io');
     const p = new Peer({ initiator: true, trickle: false });
 
-    p.on('error', err => console.log('error', err));
+    ws.on('open', () => {
+      console.log('Connected to connector.');
+      ws.send(id);
 
-    p.on('signal', data => {
-      console.log('SIGNAL', JSON.stringify(data));
-      document.querySelector('#outgoing').textContent = JSON.stringify(data);
+      p.on('error', err => console.log('error', err));
+
+      p.on('signal', data => {
+        console.log(`signal ${JSON.stringify(data)}`);
+        ws.send(
+          JSON.stringify({
+            type: 'request',
+            signal: JSON.stringify(data)
+          })
+        );
+      });
+
+      p.on('connect', () => {
+        const msg = Math.random();
+        console.log(`CONNECT passive ${msg}`);
+        p.send(`saas ${msg}`);
+      });
+
+      p.on('data', data => {
+        console.log(`data: ${data}`);
+      });
     });
 
-    document.querySelector('form').addEventListener('submit', ev => {
-      ev.preventDefault();
-      console.log('Submitted');
-      p.signal(JSON.parse(document.querySelector('#incoming').value));
-    });
-
-    p.on('connect', () => {
-      console.log('CONNECT');
-      p.send(`whatever ${Math.random()}`);
-    });
-
-    p.on('data', data => {
-      console.log(`data: ${data}`);
+    ws.on('message', data => {
+      const message = JSON.parse(data);
+      if (message.type === 'request') {
+        const p2 = new Peer({ trickle: false });
+        p2.signal(message.signal);
+        p2.on('signal', signal => {
+          ws.send(
+            JSON.stringify({
+              type: 'response',
+              signal: JSON.stringify(signal),
+              destination: message.destination
+            })
+          );
+        });
+        p2.on('connect', () => {
+          const msg = Math.random();
+          console.log(`CONNECT passive ${msg}`);
+          p2.send(`saas ${msg}`);
+        });
+        p2.on('data', () => {
+          console.log(`data: ${data}`);
+        });
+      } else if (message.type === 'response') {
+        p.signal(message.signal);
+      }
     });
   }
 
