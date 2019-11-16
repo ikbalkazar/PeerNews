b3a859b7759241b67563f34a06faa0c5e76ce363
Enes Emre Erdem <emre.erdem@ug.bilkent.edu.tr>
2019-11-16 15:27:48 +0300

    Fixed

--- source/src/PostMessage.js
+++ source/src/PostMessage.js
@@ -6,7 +6,6 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { DropdownButton, MenuItem } from 'react-bootstrap';
 import { Modal } from 'react-bootstrap';
-import { colourOptions } from '../data';
 
 export default class PostMessage extends React.Component {
 
